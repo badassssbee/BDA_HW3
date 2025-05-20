4592dc41e6ad8f661ea7c81124503aa0a8717536
badassssbee <weihsunpan450@gmail.com>
2025-05-20 21:07:24 +0800

    Update Markowitz.py

--- Markowitz.py
+++ Markowitz.py
@@ -121,6 +121,7 @@ class RiskParityPortfolio:
         """
         TODO: Complete Task 2 Below
         """
+        """
         for i in range(self.lookback, len(df)):
             returns_window = df_returns[assets].iloc[i - self.lookback:i]
             vol = returns_window.std()
@@ -133,6 +134,17 @@ class RiskParityPortfolio:
             self.portfolio_weights.loc[df.index[i]] = row_weights
 
         self.portfolio_weights[self.exclude] = 0.0
+        """
+        for i in range(self.lookback, len(df)):
+        # ❶ use the most-recent `lookback` daily returns (inclusive of today)
+        window = df_returns[assets].iloc[i - self.lookback + 1 : i + 1]
+
+        # ❷ population σ (ddof=0) → inverse-vol weights
+        vol  = window.std(ddof=0)
+        w_iv = (1.0 / vol) / (1.0 / vol).sum()
+
+        # ❸ write the weights for this date
+        self.portfolio_weights.loc[df.index[i], assets] = w_iv.to_numpy()
         """
         TODO: Complete Task 2 Above
         """
